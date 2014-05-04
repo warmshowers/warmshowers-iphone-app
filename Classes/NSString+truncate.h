1df1697dd808d994d0cb0dad031b565cf4fd2a44
Christopher Meyer <chris@rhouse.ch>
2014-05-04 15:20:22 +0200

    deleted unused code

--- Classes/NSString+truncate.h
+++
@@ -1,8 +0,0 @@
-// http://iphonedevelopertips.com/cocoa/truncate-an-nsstring-and-append-an-ellipsis-respecting-the-font-size.html
-
-@interface NSString (truncate)
-
--(NSString *)truncateToLength:(int)charLength;
--(NSString *)trim;
-
-@end
